8ab8d7d580debdd9c9924a1429349b915f747c22
Vasileios Moratis <vassi07@gmail.com>
2026-01-27 19:59:14 +0100

    Printing the hex code in color

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 import chalk from 'chalk';
 
-const number= `#${Math.floor(Math.random()*16777215).toString(16).padStart(6,"0")}`;
+const randomHex = '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
 
-
-let chalkvalue= chalk.hex(number);
-
-console.log(chalkvalue);
+console.log(chalk.hex(randomHex)(randomHex));
